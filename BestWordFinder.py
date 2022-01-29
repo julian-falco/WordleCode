59b4a7fdd7f6ee15909e4539277d257dee194e4d
Julian Falco <julian.falco@utexas.edu>
2022-01-29 02:15:43 -0600

    Updated algorithm

--- BestWordFinder.py
+++ BestWordFinder.py
@@ -17,39 +17,61 @@ def import_words(filename):
 def compare(response, answer):
     """Find the number of green and yellow squares based on a given response and answer"""
 
-    greens = 0
-    yellows = 0
-    green_letters = set() # keep track of each found green letter
+    response_letters = [[letter, ""] for letter in response]
+    answer_letters = [[letter, ""] for letter in answer]
 
-    # find green letters first
-    for i in range(len(answer)):
-        if response[i] == answer[i]:
-            greens += 1
-            green_letters.add(response[i])
+    # find green letters
+    for i in range(len(answer_letters)):
+        if response_letters[i][0] == answer_letters[i][0]:
+            response_letters[i][1] = "g"
+            answer_letters[i][1] = "g"
     
-    # find yellow letters (letter is not marked as yellow if it was found as green)
-    for i in range(len(answer)):
-        if (response[i] not in green_letters) and (response[i] in answer):
-            yellows += 1
+    # find yellow letters
+    for i in range(len(response_letters)):
+        if response_letters[i][1] == "g":
+            continue # do not check if already flagged as green
+        for j in range(len(answer_letters)):
+            if answer_letters[j][1] != "":
+                continue # do not check if already flagged
+            if response_letters[i][0] == answer_letters[j][0]:
+                if answer_letters[j][1] == "y":
+                    break
+                else:
+                    response_letters[i][1] = "y"
+                    answer_letters[j][1] = "y"
+                break
     
-    return greens, yellows
+    return response_letters
 
-def convert_to_score(greens, yellows):
+def convert_to_score(response_letters):
     """Based on green and yellow squares, create a score of how good the guess was"""
 
-    # best possible guess -- all five letters correct
-    if greens == 5:
-        return 1
-    
-    # each green square is a full point
-    points = greens
+    score = 0
 
-    # each yellow is worth 1 divided by the number of unkown squares
-    if yellows > 0:
-        unknowns = 5 - 1 - greens
-        points += yellows / unknowns
+    # count all the greens (worth one full point)
+    for letter in response_letters:
+        if letter[1] == "g":
+            score += 1
+    if score >= 4:
+        return score
     
-    return points
+    # count and score each yellow
+    for i in range(len(response_letters)):
+        if response_letters[i][1] == "y":
+            possible_spaces = 0
+            for j in range(len(response_letters)):
+                if j == i: 
+                    continue # discount if same index
+                elif response_letters[j][1] == "g": 
+                    continue # discount if letters is already found
+                elif response_letters[j][0] == response_letters[i][0]:
+                    continue # discount if same letter is found elsewhere
+                else:
+                    possible_spaces += 1
+            score += 1 / possible_spaces
+    
+    return score
+    
 
 def store_scores(list, filename):
     """Store a list of scores into a text file"""
@@ -65,12 +87,13 @@ def test_words():
     answer = input("\nanswer: ")
     response = input("response: ")
 
-    greens, yellows = compare(response, answer)
-    score = convert_to_score(greens, yellows) * 5
+    response_scored = compare(response, answer)
+    score = convert_to_score(response_scored)
 
-    print(greens, yellows, score)
+    print(response_scored, score)
 
 def main():
+
     # import lists of words
     answers = import_words("PossibleAnswers.txt")
     allowed = answers + import_words("AllowedResponses.txt") # all possible gueses is the two files combined
@@ -92,8 +115,8 @@ def main():
         # score each possible response with each known answer and add them up
         score = 0
         for answer in answers:
-            greens, yellows = compare(response, answer)
-            score += convert_to_score(greens, yellows)
+            response_letters = compare(response, answer)
+            score += convert_to_score(response_letters)
         
         # add averaged score to master list
         allowed_scored.append((score / len(answers), response))
